fd63fa1dcf99c6ef457fd0d37e6941712e78b83c
Jatin Gaur <jgaur348@gmail.com>
2022-08-10 23:06:26 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+//-------------Header scroll background----------------------
+const nav = document.querySelector("header");
+function changeBG() {
+    let scrollval = window.scrollY;
+    if (scrollY > 50) nav.classList.add("header-scroll");
+    else if ((scrollY < 50) & (hamopen == false))
+        nav.classList.remove("header-scroll");
+}
+window.addEventListener("scroll", changeBG);
+//------------------ham-button----------------------------------
+let hambtn = document.querySelector(".ham-button");
+let ul = document.querySelector(".nav-list");
+let hamopen = false;
+hambtn.addEventListener("click", function () {
+    if (hamopen == false) {
+        hambtn.classList.add("open");
+        nav.classList.add("header-scroll");
+        ul.classList.add("active");
+        hamopen = true;
+    } else {
+        hambtn.classList.remove("open");
+        ul.classList.remove("active");
+        hamopen = false;
+        if (scrollval == 0) {
+            nav.classList.remove("header-scroll");
+        }
+    }
+});
+//---------------------video-play-button-------------------
+let video = document.querySelector(".video");
+let btn = document.querySelector(".play-btn-circle");
+
+function toggleplay() {
+    if (video.paused) {
+        video.play();
+        btn.classList.add("none");
+    } else {
+        video.pause();
+        btn.classList.remove("none");
+    }
+}
+btn.onclick = function () {
+    toggleplay();
+};
+video.onclick = function () {
+    toggleplay();
+};
